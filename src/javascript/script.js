4fe4083689fbad7ca0407f810db90b34badb917d
iomuppet <suricatewesley@gmail.com>
2024-10-18 11:36:12 -0300

    adição da base do site html, css e javascript

---
+++ src/javascript/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdowns = document.getElementsByClassName("dropdown-btn");
+
+    for (var i = 0; i < dropdowns.length; i++) {
+        dropdowns[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+            } else {
+                dropdownContent.style.display = "block";
+            }
+        });
+    }
+});
