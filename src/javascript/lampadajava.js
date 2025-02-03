7df3025b1f4271896c4661fe837c5a4cc977c9ed
iomuppet <suricatewesley@gmail.com>
2025-02-03 16:37:28 -0300

    alterações no codigo js do projeto lampada

--- src/javascript/lampadajava.js
+++ src/javascript/lampadajava.js
@@ -1,31 +1,43 @@
-const turnon = document.getElementById ('turnon');
-const turnoff = document.getElementById ('turnoff');
-const lamp = document.getElementById ('lamp');
+const turnon = document.getElementById('turnon');
+const turnoff = document.getElementById('turnoff');
+const lamp = document.getElementById('lamp');
+
+let lampTurnedOnByButton = false;
 
 function lampisbroken () {
-    return lamp.src.indexOf ('lampadaquebrada') > -1
+    return lamp.src.indexOf('lampadaquebrada') > -1;
 }
 
 function lampon () {
-    if (!lampisbroken ()){
-        lamp.src = './src/images/lampadaon.png'
+    if (!lampisbroken ()) {
+        lamp.src = './src/images/lampadaon.png';
+    }
+}
+
+function lamponByButton () {
+    if (!lampisbroken ()) {
+        lamp.src = './src/images/lampadaon.png';
+        lampTurnedOnByButton = true;
+        lamp.removeEventListener('mouseleave', lampoff);
     }
 }
 
 function lampoff () {
-    if (!lampisbroken ()){
-    lamp.src = './src/images/lampadaoff.png'
+    if (!lampisbroken ()) {
+        lamp.src = './src/images/lampadaoff.png';
+        if (lampTurnedOnByButton) {
+            lamp.addEventListener('mouseleave', lampoff);
+            lampTurnedOnByButton = false;
+        }
     }
 }
 
 function lampbroken () {
-    if (!lampisbroken ()){
-    lamp.src = './src/images/lampadaquebrada.png'
-    }
+    lamp.src = './src/images/lampadaquebrada.png';
 }
 
-turnon.addEventListener ('click', lampon);
-turnoff.addEventListener ('click', lampoff);
-lamp.addEventListener ('mouseover', lampon);
-lamp.addEventListener ('mouseleave', lampoff);
-lamp.addEventListener ('dblclick', lampbroken);
+turnon.addEventListener('click', lamponByButton);
+turnoff.addEventListener('click', lampoff);
+lamp.addEventListener('mouseover', lampon);
+lamp.addEventListener('mouseleave', lampoff);
+lamp.addEventListener('dblclick', lampbroken);
